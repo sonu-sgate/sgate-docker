4584e3b6a58cebd64b9475da593333973a2f8054
khushbu singh <111412086+khushbu911@users.noreply.github.com>
2024-01-08 15:57:47 +0530

    working on docker

---
+++ docker-react/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{textAlign:"center",color:"red"}}>TO DO</h1>
+      <h3> Today What you will do your future</h3>
+      <button>Create+</button>
+      <table style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
+        <thead>
+        <tr>
+          <td>Sl no.</td>
+          <td>Name</td>
+          <td>Description</td>
+          <td>Status</td>
+          <td>Edit</td>
+          <td>Delete</td>
+          
+        </tr>
+        </thead>
+        <tbody>
+            
+            </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
