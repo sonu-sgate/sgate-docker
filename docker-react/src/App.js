13d19cfe928c59784db24afc05fa8d34600a1a3f
sonu-sgate <sgate.sonu@gmail.com>
2024-01-08 16:37:35 +0530

    docker

--- docker-react/src/App.js
+++ docker-react/src/App.js
@@ -10,7 +10,7 @@ function App() {
       <table style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
         <thead>
         <tr>
-          <td>Serial no.</td>
+          <td>Serino.</td>
           <td>Name</td>
           <td>Description</td>
           <td>Status</td>
